551b371ab8034f9528fd268160902a074a0bbd9c
meta-verse09 <m.gazali1975@gmail.com>
2026-08-29 10:17:55 +0700

    Create koa_cards.js

---
+++ koa_cards.js
@@ -0,0 +1,10 @@
+const JENIS_KARTU = [
+  "Hiu", "Jarum", "Sudung", "Bengkok",
+  "Tali", "Pecah", "Batung", "Sisir", "Babi"
+];
+
+// Contoh 1 kartu di tangan pemain
+const contohTangan = [
+  { jenis: "Hiu" }, { jenis: "Hiu" }, { jenis: "Jarum" },
+  { jenis: "Sudung" }, { jenis: "Bengkok" }
+];
