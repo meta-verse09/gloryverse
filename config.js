3610a548c081bd4df43c21e9e89de7883db671b1
meta-verse09 <m.gazali1975@gmail.com>
2026-08-30 15:31:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js
 window.SUPABASE_URL = "https://gfhcqbosxtcuvxttmzsm.supabase.co";
-window.SUPABASE_ANON = "publis key : sb_publishable_yFwUs9GLJb8PvnB5Qj8rAA_vLF32P6l";
+window.SUPABASE_ANON = "sb_publishable_yFwUs9GLJb8PvnB5Qj8rAA_vLF32P61";
 
 function initDb(){
   if(typeof supabase === 'undefined'){
